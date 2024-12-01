b900816491df3231b444619b1a41c5fffea17998
S.S.M.Chathuranganee <sandunichathurangi18@gmail.com>
2024-12-01 21:29:02 +0530

    user update

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,59 +1,105 @@
-import User from  '../models/user.js'
-import jwt from 'jsonwebtoken'
+import User from "../models/user.js";
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
+import dotenv from "dotenv"
+dotenv.config()
 
-export function postUsers(req,res){
+//---------------------------Sign Up Function--------------------------
+export function postUsers(req,res){            
+    const user = req.body;
 
-  const user = req.body
+    const password = req.body.password;       //Password hashing
+    const saltRounds = 10;
+    const passwordHash = bcrypt.hashSync(password, saltRounds);
+    user.password = passwordHash
 
-  const newUser = new User(user)
-  newUser.save().then(
-    ()=>{
-      res.json({
-        message : "User created successfully"
-      })
-    }
-  ).catch(
-    ()=>{
-      res.json({
-        message : "User creation failed"
-      })
-    }
-  )
+    const newUser = new User(user)
+    newUser.save().then(
+        ()=>{
+            res.json({
+                message : "User Created Successfully"
+            })
+        }
+    ).catch(
+        ()=>{
+            res.json({
+                message : "User Creation Failed"
+            })
+        }
+    )
 }
 
-//------------Login--------------
-export function loginUser(req,res){
-  const credentials = req.body
+//--------------------------------Sign In Function--------------------------------
+export function loginUsers(req,res){          
+    const credentials = req.body
 
-  User.findOne({email : credentials.email, password :credentials.password}).then(
-    (user)=>{
-      if(user == null){
-        res.status(403).json({
-          message : "User not found"
-        })
-      
-      }else{
-        const payload = {
-          id: user._id,
-          email: user.email,
-          firstName: user.firstName,
-          lastName: user.lastName,
-          type: user.type,
-        };
+    User.findOne({email: credentials.email}).then(
+        (user)=>{
+            if(!user){
+                res.status(403).json({
+                    message: "User not found"
+                })
+            }else{
+                const isPasswordValid = bcrypt.compareSync(credentials.password, user.password)
+                
+                if(!isPasswordValid){
+                    res.status(403).json({
+                        message: "Incorrect Password"
+                    })
+                }else{
+                    const payload = {
+                        id: user._id,
+                        email: user.email,
+                        firstName: user.firstName,
+                        lastName: user.lastName,
+                        type: user.type
+                    };
 
-        const token = jwt.sign(payload, "secret", { expiresIn: "1h" });
+                    const token = jwt.sign(payload, process.env.JWT_KEY, { expiresIn: "48h"});   //asign user information to the "token" , "secret" is the password
+                
+                    res.json({
+                        message: "User found",
+                        user: user,
+                        token: token   //send the token to the frontend
+                    })
+                }
+            }
+        }
+    )
+}
 
+//--------------------------Admin checking------------------------------
+export function isAdminValid(req){
+    if(req.user == null){
+        return false
+    }
+    if(req.user.type != "Admin"){
+        return false
+    }
+    return true
+}
+
+//-------------------------Customer checking-----------------------------
+export function isCustomerValid(req){
+    if(req.user == null){
+        return false
+    }
+    if(req.user.type != "Customer"){
+        return false
+    }
+    return true
+}
+
+export function getUser(req, res){
+    const user = req.body.user
+    if(user == null){
         res.json({
-          message : "User found",
-          user : user,
-          token : token
+            message: "not found"
         })
-      }
+    }else{
+        res.json({
+            message: "found",
+            user: user
+        })
     }
-  )
 }
-
-        
-
-
-
